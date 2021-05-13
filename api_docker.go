7e25fa6f6c0a4fb3bbbae0e919e4acaed0d431db
x1nchen <xinchenccx@gmail.com>
2021-05-13 16:47:09 +0800

    fix: format

--- api_docker.go
+++ api_docker.go
@@ -2,8 +2,6 @@ package portainer
 
 import (
 	"context"
-	"encoding/base64"
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -675,102 +673,103 @@ func (a *DockerApiService) CreateImage(ctx context.Context, registryAuthToken st
 
 // DeleteContainer
 // https://portainer.host/api/endpoints/{endpoint_id}/docker/containers/{container_id}?force=true&v=1
-func (a *DockerApiService) DeleteContainer(ctx
-context.Context, endpointId
-int32, containerID
-string) (*http.Response, error) {
-var (
-localVarHttpMethod = strings.ToUpper("Delete")
-localVarPostBody   interface{}
-localVarFileName   string
-localVarFileBytes  []byte
-)
+func (a *DockerApiService) DeleteContainer(
+	ctx context.Context,
+	endpointId int32,
+	containerID string,
+) (*http.Response, error) {
+	var (
+		localVarHttpMethod = strings.ToUpper("Delete")
+		localVarPostBody   interface{}
+		localVarFileName   string
+		localVarFileBytes  []byte
+	)
 
-// create path and map variables
-localVarPath := a.client.cfg.BasePath + fmt.Sprintf("/endpoints/%d/docker/containers/%s", endpointId, containerID)
-localVarHeaderParams := make(map[string]string)
+	// create path and map variables
+	localVarPath := a.client.cfg.BasePath + fmt.Sprintf("/endpoints/%d/docker/containers/%s", endpointId, containerID)
+	localVarHeaderParams := make(map[string]string)
 
-localVarQueryParams := url.Values{}
-localVarQueryParams.Set("force", "true")
-localVarQueryParams.Set("v", "1")
+	localVarQueryParams := url.Values{}
+	localVarQueryParams.Set("force", "true")
+	localVarQueryParams.Set("v", "1")
 
-localVarFormParams := url.Values{}
+	localVarFormParams := url.Values{}
 
-// to determine the Content-Type header
-localVarHttpContentTypes := []string{}
+	// to determine the Content-Type header
+	localVarHttpContentTypes := []string{}
 
-// set Content-Type header
-localVarHttpContentType := selectHeaderContentType(localVarHttpContentTypes)
-if localVarHttpContentType != "" {
-localVarHeaderParams["Content-Type"] = localVarHttpContentType
-}
+	// set Content-Type header
+	localVarHttpContentType := selectHeaderContentType(localVarHttpContentTypes)
+	if localVarHttpContentType != "" {
+		localVarHeaderParams["Content-Type"] = localVarHttpContentType
+	}
 
-// to determine the Accept header
-localVarHttpHeaderAccepts := []string{"application/json"}
+	// to determine the Accept header
+	localVarHttpHeaderAccepts := []string{"application/json"}
 
-// set Accept header
-localVarHttpHeaderAccept := selectHeaderAccept(localVarHttpHeaderAccepts)
-if localVarHttpHeaderAccept != "" {
-localVarHeaderParams["Accept"] = localVarHttpHeaderAccept
-}
-if ctx != nil {
-// API Key Authentication
-if auth, ok := ctx.Value(ContextAPIKey).(APIKey); ok {
-var key string
-if auth.Prefix != "" {
-key = auth.Prefix + " " + auth.Key
-} else {
-key = auth.Key
-}
-localVarHeaderParams["Authorization"] = key
+	// set Accept header
+	localVarHttpHeaderAccept := selectHeaderAccept(localVarHttpHeaderAccepts)
+	if localVarHttpHeaderAccept != "" {
+		localVarHeaderParams["Accept"] = localVarHttpHeaderAccept
+	}
+	if ctx != nil {
+		// API Key Authentication
+		if auth, ok := ctx.Value(ContextAPIKey).(APIKey); ok {
+			var key string
+			if auth.Prefix != "" {
+				key = auth.Prefix + " " + auth.Key
+			} else {
+				key = auth.Key
+			}
+			localVarHeaderParams["Authorization"] = key
 
-}
-}
-r, err := a.client.prepareRequest(ctx, localVarPath, localVarHttpMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, localVarFileName, localVarFileBytes)
-if err != nil {
-return nil, err
-}
-localVarHttpResponse, err := a.client.callAPI(r)
-if err != nil || localVarHttpResponse == nil {
-return localVarHttpResponse, err
-}
+		}
+	}
+	r, err := a.client.prepareRequest(ctx, localVarPath, localVarHttpMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, localVarFileName, localVarFileBytes)
+	if err != nil {
+		return nil, err
+	}
+	localVarHttpResponse, err := a.client.callAPI(r)
+	if err != nil || localVarHttpResponse == nil {
+		return localVarHttpResponse, err
+	}
 
-localVarBody, err := ioutil.ReadAll(localVarHttpResponse.Body)
-localVarHttpResponse.Body.Close()
-if err != nil {
-return localVarHttpResponse, err
-}
+	localVarBody, err := ioutil.ReadAll(localVarHttpResponse.Body)
+	localVarHttpResponse.Body.Close()
+	if err != nil {
+		return localVarHttpResponse, err
+	}
 
-if localVarHttpResponse.StatusCode >= 300 {
-newErr := GenericSwaggerError{
-body:  localVarBody,
-error: localVarHttpResponse.Status,
-}
+	if localVarHttpResponse.StatusCode >= 300 {
+		newErr := GenericSwaggerError{
+			body:  localVarBody,
+			error: localVarHttpResponse.Status,
+		}
 
-if localVarHttpResponse.StatusCode > 400 {
-var v model.EndpointListResponse
-err = a.client.decode(&v, localVarBody, localVarHttpResponse.Header.Get("Content-Type"))
-if err != nil {
-newErr.error = err.Error()
-return localVarHttpResponse, newErr
-}
-newErr.model = v
-return localVarHttpResponse, newErr
-}
+		if localVarHttpResponse.StatusCode > 400 {
+			var v model.EndpointListResponse
+			err = a.client.decode(&v, localVarBody, localVarHttpResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
+				return localVarHttpResponse, newErr
+			}
+			newErr.model = v
+			return localVarHttpResponse, newErr
+		}
 
-if localVarHttpResponse.StatusCode == 500 {
-var v model.GenericError
-err = a.client.decode(&v, localVarBody, localVarHttpResponse.Header.Get("Content-Type"))
-if err != nil {
-newErr.error = err.Error()
-return localVarHttpResponse, newErr
-}
-newErr.model = v
-return localVarHttpResponse, newErr
-}
+		if localVarHttpResponse.StatusCode == 500 {
+			var v model.GenericError
+			err = a.client.decode(&v, localVarBody, localVarHttpResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
+				return localVarHttpResponse, newErr
+			}
+			newErr.model = v
+			return localVarHttpResponse, newErr
+		}
 
-return localVarHttpResponse, newErr
-}
+		return localVarHttpResponse, newErr
+	}
 
-return localVarHttpResponse, nil
+	return localVarHttpResponse, nil
 }
